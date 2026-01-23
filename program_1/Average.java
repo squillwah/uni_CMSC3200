db9a661fdc67f0f87d7bc4ca6a873971dbba9692
squillwah <Swagapotato@gmail.com>
2026-01-23 13:21:39 -0500

    first

---
+++ program_1/Average.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+
+public class Average {
+    public static void main(String args[]) throws IOException {
+        System.out.println(" CMSC3200 / Group ?
+        System.out.println(" Grade Averaging Program");
+        System.out.println(" Brandon Schwartz, Joshua Staffen, Ravi Dressler");
+
+
+    }
+}
+
+
