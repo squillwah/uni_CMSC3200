d3701777df149e584c70c6dfe62e735016c504aa
squillwah <Swagapotato@gmail.com>
2026-01-31 17:43:54 -0500

    compiles and runs

--- program_2/FileCopy.java
+++ program_2/FileCopy.java
@@ -1,7 +1,7 @@
 
 import java.io.*;
 
-public class IOFileState {
+class IOFileState {
     private int status;
     private String input_file_name;
     private String output_file_name;
@@ -22,29 +22,34 @@ public class IOFileState {
     public void set_input(String file_name) { 
         input_file_name = file_name;
         status &= ~(INPUT_NOTGIVEN + INPUT_NOEXIST);
-        status |= !file_name.isEmpty() * INPUT_NOTGIVEN;
-        status |= !check_exists(file_name) * INPUT_NOEXIST;
+        if (file_name.isEmpty()) status |= INPUT_NOTGIVEN;
+        if (!check_exists(file_name)) status |= INPUT_NOEXIST;
     }
     // zzz
     public void set_output(String file_name) { 
         output_file_name = file_name;
         status &= ~(OUTPUT_NOTGIVEN + OUTPUT_DOEXIST);
-        status |= !file_name.isEmpty() * OUTPUT_NOTGIVEN;
-        status |= check_exists(file_name) * OUTPUT_DOEXIST;
+        if (file_name.isEmpty()) status |= OUTPUT_NOTGIVEN;
+        if (check_exists(file_name)) status |= OUTPUT_DOEXIST;
     }
 
     public int get_status() { return status; }
     public String get_input() { return input_file_name; }
     public String get_output() { return output_file_name; }
+
+    private static boolean check_exists(String file_name) {
+        return true; // placeholder
+    }
 }
 
 public class FileCopy {
     public static void main(String args[]) {
 
-        IOFileState files = new IOFileState("", "");
+        IOFileState files = new IOFileState("in", "out");
 
         int files_status = files.get_status();
         int files_status_exit_states = IOFileState.INPUT_NOTGIVEN;      // End program if this state is reached.
+        System.out.println(files_status);
         while ((files_status & files_status_exit_states) == 0) {
             // Fix error states if present.
             if (files_status > 0) { 
@@ -59,7 +64,11 @@ public class FileCopy {
                     if ((files_status & IOFileState.OUTPUT_DOEXIST) > 0) {
                         //
                     }
-                    files_status = files.get_status();
+
+                    // debug, clear error state and set INPUT_NOTGIVEN to test and terminate loop
+                    files_status = files.get_status() | IOFileState.INPUT_NOTGIVEN;
+                    files_status &= ~IOFileState.OUTPUT_DOEXIST;
+                    System.out.println(files_status);
                 }
                 continue;
             }
