8e53f4c197f515fbbdd218149f4f5b3e9b5c374d
squillwah <Swagapotato@gmail.com>
2026-01-31 13:32:57 -0500

    outlining main algorithm loop

--- program_2/FileCopy.java
+++ program_2/FileCopy.java
@@ -31,10 +31,51 @@ public class IOFileState {
     }
 
     public int get_status() { return status; }
+
+    private static boolean check_exists(String file_name);
+
+    // should IOFileState also open file streams, or is that outside its scope?
 }
 
 public class FileCopy {
     public static void main(String args[]) {
+
+        IOFileState files = new IOFileState();
+
+        int files_status = files.get_status();
+        while (!(files_status & IOFileState.INPUT_NOTGIVEN)) {      // Blank input file is exit condition.
+            while (files_status) {      // Correct error states, if any.
+                if (files_status & IOFileState.INPUT_NOEXIST) {
+                    // Reprompt for input
+                    // @bug: How can we quit from here? Empty input won't break the loop.
+                    // zzz
+                }
+                if (files_status & IOFileState.OUTPUT_NOTGIVEN) {
+                    // Reprompt for output
+                }
+                if (files_status & IOFileState.OUTPUT_DOEXIST) {
+                    // Prompt for reentry, backup/overwrite, or quit
+                }
+                files_status = files.get_status();
+
+
+                // @bug: Potential stuck state here, if INPUT_NOTGIVEN flipped and bypassed the outer loop.
+                //        Dunno how that could happen though, excluding cosmic radiation.
+                //        Although, maybe not if INPUT_NOEXIST is always set when INPUT_NOTGIVEN is set. Should they be the same? No, probs not.
+            }
+
+            // Copy files and do the thing
+
+            // Get new input/output and refresh status
+            files.set_input(/*prompt*/); 
+            files.set_input(/*prompt*/); // might want to find some way to merge these prompts with initial?
+
+            files_status = files.get_status(); // feels weird to call this in twice, but no other way?
+        }
+
+
+
+        /* old
         // Parse argument array for valid file names
         
         int arguments_status = ArgStatus.parse_args(args);
