b625c3a43ccd4a012dcbf370d53cb578f26f6906
squillwah <Swagapotato@gmail.com>
2026-02-05 17:52:31 -0500

    fixed typo

--- program_2/FileCopy.java
+++ program_2/FileCopy.java
@@ -14,8 +14,8 @@ import java.io.*;
 /* ======================================================================================
  * CLog
  *
- *  Wrapper for printing to System.out.
- *  Provides toggles & formatting for plain/log/error/warning messages.
+ *  Wrapper for System.out.print
+ *  Provides toggles & formatting for plain, log, error, & warning messages.
  *
  *  Methods:
  *   - set_mode(int m) 
@@ -54,9 +54,9 @@ class CLog {
         System.out.println("> " + msg);
     }
 
-    public static void log(String msg) { if (mode & LOGS) sayln("log: " + msg) }
-    public static void warn(String msg) { if (mode & WARNINGS) sayln("war: " + msg) }
-    public static void error(String msg) { if (mode & ERRORS) sayln("err: " + msg) }
+    public static void log(String msg) { if ((mode & LOGS) > 0) sayln("log: " + msg); }
+    public static void warn(String msg) { if ((mode & WARNINGS) > 0) sayln("war: " + msg); }
+    public static void error(String msg) { if ((mode & ERRORS) > 0) sayln("err: " + msg); }
 }
 
 
@@ -73,23 +73,24 @@ class CLog {
  * ====================================================================================== */
 
 class FileTools {
-    private FileTools();
+    private FileTools() {}
     
     public static void backup_file(String file_name) {
         //
     }
     
     public static boolean file_exists(String file_name) {
-
+        return (new File(file_name)).exists();
     }
     
     public static BufferedReader open_if(String file_name) {
-
+        return null;
     }
     
     public static PrintWriter open_of(String file_name) {
-
+        return null;
     }
+
 }
 
 
@@ -105,13 +106,13 @@ class FileTools {
  * ====================================================================================== */
 
 class WordTools {
-    private WordTools();
+    private WordTools() {}
     
     public static boolean is_int(String word) {
         boolean is = true;
         try {
             Integer.parseInt(word);
-        } catch (NumberFormatExtension e) {
+        } catch (NumberFormatException e) {
             is = false;
         }
         return is;
@@ -169,20 +170,20 @@ class IOFileState {
         input_file = file_name;
         status &= ~(INPUT_NOTGIVEN + INPUT_NOEXIST + OUTPUT_IS_INPUT);
         if (file_name.isEmpty()) status |= INPUT_NOTGIVEN;
-        else if (!File(file_name).exists()) status |= INPUT_NOEXIST;
+        else if (!FileTools.file_exists(file_name)) status |= INPUT_NOEXIST;
         else if (input_file == output_file) status |= OUTPUT_IS_INPUT;
     }
     public void set_output(String file_name) { 
-        output_file_name = file_name;
+        output_file = file_name;
         status &= ~(OUTPUT_NOTGIVEN + OUTPUT_DOEXIST + OUTPUT_IS_INPUT);
         if (file_name.isEmpty()) status |= OUTPUT_NOTGIVEN;
-        else if (!File(file_name).exists()) status |= OUTPUT_DOEXIST;
-        else if (input_file == output_fille) status |= OUTPUT_IS_INPUT;
+        else if (FileTools.file_exists(file_name)) status |= OUTPUT_DOEXIST;
+        else if (input_file == output_file) status |= OUTPUT_IS_INPUT;
     }
 
     public int get_status() { return status; }
-    public String get_input() { return input_file_name; }
-    public String get_output() { return output_file_name; }
+    public String get_input() { return input_file; }
+    public String get_output() { return output_file; }
 }
 
 
@@ -195,7 +196,31 @@ class IOFileState {
 
 public class FileCopy {
     public static void main(String args[]) {
-        // Construct IOFileState from args.
+       
+        // Get file names from command line 
+        IOFileState files = args_to_iofs(args);
+        int files_status = files.get_status();   
+
+        // Reprompt file names if not given or invalid
+        if (files_status != 0) {
+            reprompt_iofs(files); 
+            files_status = files.get_status();
+        }
+        
+        // Proceed with tokenization & file copy if status is nominal.
+        if (files_status == 0) {  
+ 
+        } 
+        // Otherwise, user chose a quit option during reprompt 
+        else { 
+            CLog.log("Cancelling FileCopy with state '" + files_status + "'.");
+        }
+        
+        CLog.sayln("Exiting program, goodbye!");
+    }
+
+    // Construct an IOFileState instance from args.
+    public static IOFileState args_to_iofs(String args[]) {
         IOFileState files = null;
         switch (args.length) {
             case 0:
@@ -208,41 +233,87 @@ public class FileCopy {
                 files = new IOFileState(args[0], args[1]);
                 break;
         }
+        return files;
+    }
 
-        // Attempt to fix error states.
+    // Examine IOFileState and reprompt stdin as needed to ensure valid filenames or a program exit.
+    public static void reprompt_iofs(IOFileState files) {
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        
         int files_status = files.get_status();
         int files_status_exit_states = 0;
-        while ((files_status & ~files_status_exit_states) > 0) {    // Disregard exit states. 
-            // Handle empty file names on first occurences, then set as exit states for future occurences.
+        while ((files_status & ~files_status_exit_states) > 0) {        // Disregard exit states. 
+            // Handle empty file names on first occurences.
             if ((files_status & IOFileState.INPUT_NOTGIVEN) > 0) {
-                files_status_exit_states |= IOFileState.INPUT_NOTGIVEN;
+                CLog.warn("No input file provided");
+                CLog.say("Please enter a valid input file name, or nothing to quit: ");
+                try {
+                    files.set_input(stdin.readLine());
+                    files_status_exit_states |= IOFileState.INPUT_NOTGIVEN; // Set as exit states for future occurences.
+                } catch (IOException e) { CLog.error("An IOException occurred during input file input: " + e); }
             }
+            
             if ((files_status & IOFileState.OUTPUT_NOTGIVEN) > 0) {
-                files_status_exit_states |= IOFileState.OUTPUT_NOTGIVEN;
+                CLog.warn("No output file provided");
+                CLog.say("Please enter a valid output file name, or nothing to quit: ");
+                try {
+                    files.set_output(stdin.readLine());
+                    files_status_exit_states |= IOFileState.OUTPUT_NOTGIVEN;
+                } catch (IOException e) { CLog.error("An IOException occurred during output file input: " + e); }
             }
+            
             // Reprompt non-existent input files.
             if ((files_status & IOFileState.INPUT_NOEXIST) > 0) {
-                //
+                CLog.warn("The input file '" + files.get_input() + "' doesn't exist");
+                files.set_input("");
+                files_status_exit_states &= ~IOFileState.INPUT_NOTGIVEN; 
             }
+            
             // Reprompt pre-existent output files, allow backup+overwrite, or quit.
             if ((files_status & IOFileState.OUTPUT_DOEXIST) > 0) {
-                if (quit) {
-                    files_status_exit_states |= IOFileState.OUTPUT_DOEXIST; // or files_status_exit_states = files.get_status(); for a more clear "exit now"
+                CLog.warn("The output file '" + files.get_output() + "' already exists");
+                CLog.sayln("It appears that output file already exists, would you like to: ");
+                CLog.sayln(" 1. Enter a different file name\n 2. Backup that file and proceed\n 3. Quit");
+                
+                String choice = null;
+                boolean waiting_for_choice = true;
+                while (waiting_for_choice) {
+                    try { choice = stdin.readLine(); }
+                    catch (IOException e) { CLog.error("An IOException occurred during choice: " + e); }
+
+                    switch (choice) {
+                        case "1":
+                            //CLog.say("Enter a different file name: ");
+                            files.set_output("");
+                            files_status_exit_states &= ~IOFileState.OUTPUT_NOTGIVEN; // Set output blank and clear NOTGIVEN from exit states to reroll.
+                            //files.set_output(stdin.readLine());
+                            waiting_for_choice = false;
+                            break;
+                        case "2":
+                            FileTools.backup_file(files.get_output());
+                            // could remove original, just rename original to .bak, or find a way to force the overwrite
+                            waiting_for_choice = false;
+                            break;
+                        case "3":
+                            CLog.sayln("Quit it is.");
+                            files_status_exit_states |= IOFileState.OUTPUT_DOEXIST; // or files_status_exit_states = files.get_status(); for a more clear "exit now"
+                            waiting_for_choice = false;
+                            break;
+                        default:
+                            CLog.sayln("Please enter '1', '2', or '3'");
+                            break;
+                    }
                 }
             }
             files_status = files.get_status();
-        }
+        }        
         
-        // Proceed with tokenization/file copy if status is nominal.
-        if (files_status == 0) {  
- 
-        } else if (files_status & files_status_exit_states > 0) {                               // Exit state encountered, program shutdown is deliberate.
-            CLog.log("Cancelling FileCopy.");
-        } else {
-            CLog.error("Couldn't recover from error state '" + files_status + "'.");   // Non-exit error state maintained, shutdown unintended.
+        if (files_status != 0) {  
+            // Exit state encountered, program shutdown is deliberate.
+            if ((files_status & files_status_exit_states) > 0) CLog.log("Cancelling FileCopy.");
+            // Non-exit error state maintained, shutdown unintended.
+            else CLog.error("iofs reprompt couldn't recover from error state '" + files_status + "'.");   
         }
-        
-        CLog.sayln("Exiting program, goodbye!");
     }
 }
 
