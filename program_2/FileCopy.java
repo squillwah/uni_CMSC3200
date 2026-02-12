b130011c0a75e3abc48dce267b33092e05157be2
squillwah <Swagapotato@gmail.com>
2026-02-11 20:10:44 -0500

    clarifying comments

--- program_2/FileCopy.java
+++ program_2/FileCopy.java
@@ -3,7 +3,7 @@
 // Program 2: FileCopy
 //
 //  FileCopy program.
-//  Parse and count the words/digits in an input file, write statistics to an output file.
+//  Parse and count the words/digits in an input file, write findings to an output file.
 //
 // Group 2
 // Brandon Schwartz, DaJuan Bowie, Joshua Staffen, Ravi Dressler
@@ -17,15 +17,16 @@ import java.util.*;
  *
  *  Defines functions for file operations needed in this program.
  *
- *  Methods:
- *   - backup(String file_name)
- *      Makes a backup of the given file, appended with .bak
- *      If a backup already exists, it creates another backup of the backup.
+ *   - rename_file(String, String)
+ *   - file_exists(String)
+ *   - open_ifstream()/open_ofstream(String)
+ *   - close_ifstream(BufferedReader)/close_ofstream(PrintWriter)
  *
  * ====================================================================================== */
 class FileTools {
     private FileTools() {}
-    
+   
+    // Rename files given name and new name strings. Recursively appends .bak to new_names which exist. 
     public static void rename_file(String old_name, String new_name) {
         if (file_exists(new_name)) {
             System.out.println("File '" + new_name + "' already exists, backing up to '" + new_name + ".bak'.");
@@ -34,15 +35,15 @@ class FileTools {
 
         // rename the file
     }
-    
-    //  POTENTIAL MEMORY LEAK IN VERY NEICHE EDGE CASE  
+   
+    // Return true if file corresponding to file_name exists, false if not. 
     public static boolean file_exists(String file_name) {
         return (new File(file_name)).exists();
     }
-    
+   
+    // Open BufferedReaders & PrintWriters given filename. Catch and describe IOExceptions if they occur.
     public static BufferedReader open_ifstream(String file_name) {
         BufferedReader inFile = null;
-
         try {
             inFile = new BufferedReader(new FileReader(file_name));
         } catch (IOException e) {
@@ -53,10 +54,8 @@ class FileTools {
         }
         return inFile;
     }
-
     public static PrintWriter open_ofstream(String file_name) {
         PrintWriter outFile = null;
-
         try {
             outFile = new PrintWriter(new FileWriter(file_name));
         } catch (IOException e) {
@@ -66,15 +65,11 @@ class FileTools {
         return outFile;
     }
 
+    // Close PrintWriters/BufferedReaders, catch and describe IOExceptions when thrown.
     public static boolean close_ofstream(PrintWriter ofstream) {
         boolean closed = false;
-        //try {                     not needed
-            ofstream.close(); 
-            closed = true;
-        //}
-        //catch (IOException e) { 
-        //    System.out.println("Err: IOException during input file close: " + e); // Should we continue?
-        //}
+        ofstream.close(); 
+        closed = true;
         return closed;
     }
     public static boolean close_ifstream(BufferedReader ifstream) {
@@ -84,25 +79,25 @@ class FileTools {
             closed = true;
         }
         catch (IOException e) { 
-            System.out.println("Err: IOException during output file close: " + e); // Should we continue?
+            System.out.println("Err: IOException during output file close: " + e); 
         }
         return closed;
     }
 
 }
 
-/* ======================================================================================
- * WordTools
- *
- *  Word processing procedures.
- *
- *  Methods:
- *   - is_int(String word)
- *      Returns true if given string is an integer, false otherwise.
- *
- * ====================================================================================== */
+// ======================================================================================
+// WordTools
+//
+//  Word processing procedures.
+//
+//   - is_int(String)
+//   - normalize_line(String)
+//   - find_word(String, Word[], int)
+//
+// ====================================================================================== */
 class WordTools {
-    private WordTools() {} // Should we have these empty constructors for purely functional classes?
+    private WordTools() {}
     
     public static boolean is_int(String word) {
         boolean is = true;
@@ -120,10 +115,7 @@ class WordTools {
         return line_clean;
     }
 
-//    // Safely read line from a BufferedReader, return normalized string.
-//    public static String read_normalized_line(BufferedReader buffer)
-//        safe_buffered_read
-
+    // Return index of Word object in words array matching string
     public static int find_word(String word, Word[] words, int words_size) {
         int index = -1;
         for (int i = 0; i < words_size && index < 0; i++)
@@ -137,6 +129,13 @@ class WordTools {
     }
 }
 
+// ============================================================================
+// Word
+//
+//  Class for words. Stores text string and occurence count.
+//  Supports incrementing the count, getting string, and getting count.
+//
+// ============================================================================
 class Word {
     String word;
     int count;
@@ -154,30 +153,26 @@ class Word {
     public int get_count() { return count; }
 }
 
-/* ======================================================================================
- * IOFileNameState
- *  
- *  Takes filename strings and evaluates them.
- *  Provides functionality to signal and explain why an input/output filename is invalid.
- *  
- *  Does checks on file names:
- *   - Was a filename given?
- *   - Does a corresponding file exist?
- *   - Are the input and output filenames different?
- *  
- *  Maintains a 'status' integer, whose bits represent each check.
- *   - Failed checks result in a positively flipped bit.
- *   - The nominal state is a status of '0', all checks passed.
- *
- *  Methods:
- *   - set_<input/output>(String file_name)
- *      Set the interal input or output file name and run checks.
- *   - get_<input/output>()
- *      Return the internal input/output file name.
- *   - get_status()
- *      Returns the status code.
- *
- * ====================================================================================== */
+// ======================================================================================
+// IOFileNameState
+//  
+//  Takes filename strings and evaluates them.
+//  Provides functionality to signal and explain why an input/output filename is invalid.
+//  Does checks on file names:
+//   Was a filename given?
+//   Does a corresponding file exist?
+//   Are the input and output filenames different?
+//  Maintains a 'status' integer, whose bits represent each check.
+//   Failed checks result in a positively flipped bit.
+//   The nominal state is a status of '0', all checks passed.
+//  
+//   - set_input(String)set_output(String): set internal filenames
+//   - get_input()/get_output(): return internal filenames 
+//   - check_input()/check_output(): run checks on internal names
+//   - clear_input()/clear_output(): set internal names blank
+//   - get_status(): Returns the status code.
+//
+// ======================================================================================
 class IOFileNameState {
     private int status;
     private String input_file;
@@ -210,14 +205,14 @@ class IOFileNameState {
         status &= ~(INPUT_NOTGIVEN + INPUT_NOEXIST + OUTPUT_IS_INPUT);
         if (input_file.isEmpty()) status |= INPUT_NOTGIVEN;
         else if (!FileTools.file_exists(input_file)) status |= INPUT_NOEXIST;
-        //else if (input_file.equals(output_file)) status |= OUTPUT_IS_INPUT; // could comparing against initial null output_file cause issues?
+        else if (input_file.equals(output_file)) status |= OUTPUT_IS_INPUT;
     }
     public void check_output() {
         status &= ~(OUTPUT_NOTGIVEN + OUTPUT_DOEXIST + OUTPUT_IS_INPUT);
         if (output_file.isEmpty()) status |= OUTPUT_NOTGIVEN;
         else if (FileTools.file_exists(output_file)) {
             status |= OUTPUT_DOEXIST;
-            if (input_file.equals(output_file)) status |= OUTPUT_IS_INPUT; 
+            if (output_file.equals(input_file)) status |= OUTPUT_IS_INPUT; 
         }
         
     }
@@ -243,11 +238,16 @@ class IOFileNameState {
 //
 // ======================================================================================================
 class Prompts {
-    private static BufferedReader stdin = null;
+    private Prompts() {}
     
+    private static BufferedReader stdin = null;
+   
+    // Open/close standard input for prompt input.  
+    // Do not call a prompt without opening stdin first.
     public static void open_stdin() {
         stdin = new BufferedReader(new InputStreamReader(System.in));
     }
+    // Only close stdin once.
     public static void close_stdin() {
         try {
             stdin.close();
@@ -257,6 +257,7 @@ class Prompts {
         }
     }
 
+    // To fix blank filenames.
     public static int no_input(IOFileNameState filenames) {
         int quit_states = 0;
         filenames.set_input(catchLine("Please specify an input file (or nothing to quit): "));
@@ -270,16 +271,16 @@ class Prompts {
         return quit_states;
     }
 
-    // Alternatively could handle the reinput itself, but currently clears input_file (throwing the prompt to no_input later on)
+    // To fix a nonexistent input file.
     public static int bad_input(IOFileNameState filenames) {
         int quit_states = 0;
         System.out.println("The input file '" + filenames.get_input() + "' doesn't exist.");
-        //filenames.clear_input(); // Resets INPUT_NOEXIST and sets INPUT_NOTGIVEN, allow Prompts.no_input() to handle reentry.
         filenames.set_input(catchLine("Please enter a different one (or nothing to quit): "));
         quit_states = filenames.get_status() & IOFileNameState.INPUT_NOTGIVEN;
         return quit_states;
     }
 
+    // To fix a preexisting output file (multiple options).
     public static int bad_output(IOFileNameState filenames) {
         int quit_states = 0;
         System.out.println("The output file '" + filenames.get_output() + "' already exists.");
@@ -308,6 +309,7 @@ class Prompts {
         return quit_states;
     }
 
+    // To fix an output file that is the input file.
     public static int io_same(IOFileNameState filenames) {
         int quit_states = 0;
         System.out.println("Your input file and output file are the same ('" + filenames.get_input() + "').");
@@ -317,7 +319,7 @@ class Prompts {
     }
    
     // Helper for getting input with a prompt. 
-    // Prints prompt, opens stdin, reads a line, closes stdin, and catches any IOExceptions. 
+    // Prints prompt, reads a line, and catches any IOExceptions. 
     private static String catchLine(String prompt) {
         System.out.print(prompt);
         System.out.flush();
@@ -331,28 +333,14 @@ class Prompts {
     }
 }
 
-// Could have a more complex OOP class
-/*
-class WordsAndSum {
-    public static final int VOCAB_SIZE = 100;
-   
-    private int integer_sum; 
-    private int word_count;
-    private Word words[];
-
-    public WordsAndSum() {
-        integer_sum = 0;
-        word_count = 0;
-        words = new Word[VOCAB_SIZE];
-    }
-
-    public int get_integer_sum() { return integer_sum; }
-    public int get_word_count() { return word_count; }
-
-    public int add_word(
-}*/
-
-// Or simple data container
+// ============================================================================
+// FileWordData
+//
+//  Container for data parsed from input files.
+//  Holds the sum of integers, array of word tokens, and the parsed word count.
+//  Can only store MAX_WORDS words.
+//
+// ============================================================================
 class FileWordData {
     public static final int MAX_WORDS = 100;
     public int sum;
@@ -366,14 +354,16 @@ class FileWordData {
     }
 }
 
-
-/* ======================================================================================
- * FileCopy
- *
- *  Parses words/numbers in input file and prints occurence count to output file.
- *  Filenames can be supplied as arguments or prompted during runtime.
- *
- * ====================================================================================== */
+// ============================================================================
+// FileCopy
+//
+//  Processes words/integers from input file, prints results to output file.
+//  Filenames can be supplied as arguments or prompted during runtime.
+//
+//   - main(): Processes filenames, opens/closes files, writes to output file.
+//   - process_input_file(BufferedReader): Parses FileWordData from ifstream.
+//
+// ============================================================================ 
 public class FileCopy {
     public static void main(String args[]) {
         // Create a filename state object, with or without defaults from command line. 
@@ -443,13 +433,13 @@ public class FileCopy {
 
         String line = null;
         try { line = inFile.readLine(); } 
-        catch (IOException e) { System.out.println("Err: couldn't first line from input file."); }
+        catch (IOException e) { System.out.println("Error reading first line from input file: " + e); }
 
         while (line != null) {
             // Debug, just print the line
             System.out.println(line);
             try { line = inFile.readLine(); } 
-            catch (IOException e) { System.out.println("Err: couldn't line after '" + line + "' from input file."); }
+            catch (IOException e) { System.out.println("Error reading next line after '" + line + "' from input file: " + e); }
             
             // @todo: the tokenize, parse words from numbers, store in WordsAndSum 
             /*String delimmiters = "\t\n\r";
