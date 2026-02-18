fa462c8f7717502b91d69610fa92a7306b4c12ec
UwUBotter <jstaffenpersonal@gmail.com>
2026-02-17 20:44:58 -0500

    Changed from Swing to AWT

--- program_3/FileCopyGui.java
+++ program_3/FileCopyGui.java
@@ -1,39 +1,56 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
 public class FileCopyGui {
 
-    public static void main(String[] args) {
-        Window win = new Window("Test", 500, 500);
+    private static final long serialVersionUID = 1L;
 
+    public static void main(String[] args) {
+        Window w = new Window();
     }
 }
 
 //  handles all GUI init and updates
-class Window {
-    //  frame data
-    private JFrame frame;
+class Window extends Frame implements WindowListener {
 
-    //  label data
+    Label title = new Label("Test");
 
-    //  gridbag data
-    private GridBagLayout gbl;
-    private GridBagConstraints gbc;
-    
+    public Window() {
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        GridBagLayout gbl = new GridBagLayout();
 
-    public Window(String title, int x, int y) {
+        double colWeight[] = {1};
+        double rowWeight[] = {1};
+        int colWidth[] = {1};
+        int rowHeight[] = {1};
 
-        //  frame construction
-        this.frame = new JFrame();
-        this.frame.setTitle(title);
-        this.frame.setSize(x, y);
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setVisible(true);
+        gbl.rowHeights = rowHeight;
+        gbl.columnWidths = colWidth;
+        gbl.rowWeights = rowWeight;
+        gbl.columnWeights = colWeight;
 
-        //  gridbag setup
-        this.gbl = new GridBagLayout();
-        this.gbc = new GridBagConstraints();
+        this.setBounds(20,20,200,100);
+        this.setLayout(gbl);
+        gbl.setConstraints(title, gbc);
+        this.add(title);
+        this.addWindowListener(this);
+        this.setVisible(true);
+
     }
+
+    //  window listeners
+    public void windowClosing(WindowEvent e) {
+
+        this.removeWindowListener(this);
+        this.dispose();
+    }
+
+    public void windowActivated(WindowEvent e) {}
+    public void windowClosed(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
+    public void windowOpened(WindowEvent e) {}
 }
