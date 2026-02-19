bbb8168ba6bb58fe378652fe9b901f2882a4e910
UwUBotter <jstaffenpersonal@gmail.com>
2026-02-19 13:19:21 -0500

    gui test working, needs work

--- program_3/FileCopyGui.java
+++ program_3/FileCopyGui.java
@@ -23,27 +23,36 @@ class Window extends Frame implements WindowListener, ActionListener {
     Button confirm;
 
     //  labels
-    Label title;    //  POORLY NAMED RN IS NOT THE TITLE
+    Label source;
+    Label currSource;
+    Label currTarget;
+    Label fileName;
 
     public Window() {
 
         //  establishing how buttons and labels go onto the screen
-        double colWeight[] = {1};   //  MESSING WITH THESE, DONT HAVE AN
-        double rowWeight[] = {1};   //  INTUITIVE FEEL FOR EM
-        int colWidth[] = {1};
-        int rowHeight[] = {1};
+        double colWeight[] = {1,10,1};   //  MESSING WITH THESE, DONT HAVE AN
+        double rowWeight[] = {12,1,1,1};   //  INTUITIVE FEEL FOR EM
+        int colWidth[] = {1,10,1};
+        int rowHeight[] = {12,1,1,1};
 
         gbl.rowHeights = rowHeight;
         gbl.columnWidths = colWidth;
         gbl.rowWeights = rowWeight;
         gbl.columnWeights = colWeight;
 
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+
         //  setting up frame settings
-        initFrame();
         this.setBounds(20,20,500,500);
         this.setLayout(gbl);
         this.addWindowListener(this);
-        this.setLocationRelativeTo(null);
+        this.setLocationRelativeTo(null);   //  setting starting pos to center screen, likes to start on my left monitor and the fix isnt universal so did this
         this.setVisible(true);
 
     }
@@ -72,15 +81,48 @@ class Window extends Frame implements WindowListener, ActionListener {
         //  buttons
         target = new Button("Target: ");
         target.addActionListener(this);
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbl.setConstraints(target, gbc);
+        this.add(target);
+
         confirm = new Button("Confirm");
         confirm.addActionListener(this);
+        gbc.gridx = 3;
+        gbc.gridy = 4;
+        gbl.setConstraints(confirm, gbc);
+        this.add(confirm);
 
         //  labels
-        title = new Label("Test");
+        source = new Label("Source: ");
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbl.setConstraints(source, gbc);
+        this.add(source);
 
-        //  setup window
-        gbl.setConstraints(title, gbc);
-        this.add(title);
+        currSource = new Label("THIS IS A TEST OF THE CURRENT SOURCE LABEL");
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbl.setConstraints(currSource, gbc);
+        this.add(currSource);
+
+        currTarget = new Label("THIS IS A TEST OF THE CURRENT TARGET LABEL");
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        gbl.setConstraints(currTarget, gbc);
+        this.add(currTarget);
+
+        fileName = new Label("File Name: ");
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        gbl.setConstraints(fileName, gbc);
+        this.add(fileName);
+        
+    }
+
+    //  update labels to reflect current selections
+    public void updateLabels() {
+
     }
 
     public void actionPerformed(ActionEvent e) {
