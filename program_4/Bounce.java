30f5c66ebff5dd88ba2e99ae5fa4fd5a46501b12
squillwah <Swagapotato@gmail.com>
2026-03-12 22:18:58 -0400

    Added characters to the file. Deleted some too.

--- program_4/Bounce.java
+++ program_4/Bounce.java
@@ -26,7 +26,7 @@ public class Bounce extends Frame implements WindowListener, ComponentListener, 
     // Control panel components.
     private Button bt_start, bt_shape, bt_clear, bt_tail, bt_quit;
     private Scrollbar sb_tickrate, sb_velocity, sb_size;
-    private Label sb_speed_lbl, sb_size_lbl;
+    private Label sb_tickrate_lbl, sb_velocity_lbl, sb_size_lbl;
 
     // The bouncing body canvas. 
     private BounceSim bsim;
@@ -80,12 +80,17 @@ public class Bounce extends Frame implements WindowListener, ComponentListener, 
         // Scrollbar value setting:
         Scrollbar[] bars = {sb_size, sb_tickrate, sb_velocity};
         for (Scrollbar bar : bars) { bar.setMaximum(120); bar.setMinimum(1); bar.setUnitIncrement(5); bar.setBlockIncrement(10); bar.setVisibleAmount(20); bar.setBackground(Color.gray); }
+        
         //sb_speed.setValue((int)(gradiate_sim_delay(.5)*100));    // Attempt to set scrolls at 50%.
         sb_tickrate.setValue(50);    // Attempt to set scrolls at 50%.
-        sb_speed_lbl = new Label((""), Label.CENTER);   add(sb_speed_lbl);  update_speed_label();
+        sb_tickrate_lbl = new Label((""), Label.CENTER);    add(sb_tickrate_lbl);   update_tickrate_label();
+        
+        sb_velocity.setValue(1);
+        sb_velocity_lbl = new Label((""), Label.CENTER);    add(sb_velocity_lbl);   update_velocity_label();
+        
         sb_size.setValue((int)(gradiate_body_size(.5)*100));
         sb_size_lbl = new Label((""), Label.CENTER);    add(sb_size_lbl);   update_size_label();
-        sb_velocity.setValue(1);
+        
         // Add self as component/window event listener. Always do this last.
         this.addComponentListener(this);
         this.addWindowListener(this);
@@ -103,12 +108,14 @@ public class Bounce extends Frame implements WindowListener, ComponentListener, 
         // Tickrate bar        
         sb_tickrate.setLocation(conpan_x, conpan_y);
         sb_tickrate.setSize(dim_scroll_w, dim_scroll_h);
-        sb_speed_lbl.setLocation(conpan_x, conpan_y + dim_scroll_h);
-        sb_speed_lbl.setSize(dim_scroll_w, dim_scroll_h);
+        sb_tickrate_lbl.setLocation(conpan_x, conpan_y + dim_scroll_h);
+        sb_tickrate_lbl.setSize(dim_scroll_w, dim_scroll_h);
         conpan_x += dim_scroll_w + conpan_sepa;
         // Velocity bar 
         sb_velocity.setLocation(conpan_x, conpan_y);
         sb_velocity.setSize(dim_scroll_w, dim_scroll_h);
+        sb_velocity_lbl.setLocation(conpan_x, conpan_y + dim_scroll_h);
+        sb_velocity_lbl.setSize(dim_scroll_w, dim_scroll_h);
         conpan_x += dim_scroll_w + conpan_sepa;
         // Buttons
         Button[] butts = {bt_start, bt_shape, bt_tail, bt_clear, bt_quit};
@@ -168,7 +175,7 @@ public class Bounce extends Frame implements WindowListener, ComponentListener, 
             bsim.set_circle(!bsim.is_circle());
             if (bsim.is_circle()) bt_shape.setLabel("Square");
             else bt_shape.setLabel("Circle");
-            bsim.repaint();   // Force repaint to update shape.
+            //bsim.repaint();   // Force repaint to update shape.   // will be HANDLED IN RUN!
         } else
         if (source == bt_tail) {
             bsim.set_tail(!bsim.has_tail());
@@ -190,10 +197,11 @@ public class Bounce extends Frame implements WindowListener, ComponentListener, 
         if (bar == sb_tickrate) {
             //bar.setValue((int)(gradiate_sim_delay(bar.getValue()/100.0)*100));
             bsim.sim_set_tickrate(bar.getValue());
-            update_speed_label();
+            update_tickrate_label();
         } else
         if (bar == sb_velocity) {
-
+            bsim.body_set_velocity_multiplier(bar.getValue()/10.0);       //temp
+            update_velocity_label();
         } else
         if (bar == sb_size) {
             // Updates value within screen, and sets scrollbar to the returned percentage (size may be restricted).
@@ -230,11 +238,15 @@ public class Bounce extends Frame implements WindowListener, ComponentListener, 
         System.out.println("Size: " + bsim.body_get_size() + " | " + ((double)(bsim.body_get_size()-bsim.BODY_SIZE_MIN) / (bsim.BODY_SIZE_MAX-bsim.BODY_SIZE_MIN)) + "%");
         return (double)(bsim.body_get_size()-bsim.BODY_SIZE_MIN) / (bsim.BODY_SIZE_MAX-bsim.BODY_SIZE_MIN);
     }
-    private void update_speed_label() {
+    private void update_tickrate_label() {
         int val = sb_tickrate.getValue();
-        if (val == 100) sb_speed_lbl.setText(("Speed (MAX%)"));
-        else if (val == 1) sb_speed_lbl.setText(("Speed (MIN%)"));
-        else sb_speed_lbl.setText(("Speed (" + val + "%)"));
+        if (val == 100) sb_tickrate_lbl.setText(("Speed (MAX%)"));
+        else if (val == 1) sb_tickrate_lbl.setText(("Speed (MIN%)"));
+        else sb_tickrate_lbl.setText(("Tickrate: " + val + "tps"));
+    }
+    private void update_velocity_label() {
+        int val = sb_velocity.getValue();
+        sb_velocity_lbl.setText("Velocity: " + bsim.body_get_velocity() + "ppt");
     }
     private void update_size_label() {
         int val = sb_size.getValue();
@@ -266,6 +278,7 @@ class BounceSim extends Canvas implements Runnable {
     private int sim_tickrate; 
     private Thread sim_thread;
     private boolean sim_paused;
+    private boolean sim_running;
 
     // Render flags: 
     private boolean render_clear;   // Clear the canvas on next draw. Resets self.
@@ -287,6 +300,9 @@ class BounceSim extends Canvas implements Runnable {
         
         sim_tickrate = 10;
         sim_delay = 1000/sim_tickrate;
+        sim_thread = new Thread(this);
+        sim_paused = true;
+        sim_running = true; // Does ruberic say sim mussed be paused or stopped on program start? Is it bad to start the thread here?
 
         size = BODY_SIZE_MIN;
         pos = new Vec2(screen_width/4, screen_height/4);
@@ -300,12 +316,11 @@ class BounceSim extends Canvas implements Runnable {
         notail_size = size;
         notail_circle = render_circle;
         
-        sim_thread = new Thread(this);
         sim_thread.start();
     }
 
     public void run() {
-        for (int i = 0; i < 1000; i++) {
+        while (sim_running) {
             //last_pos_x = (int)Math.round(pos.x);
             //last_pos_y = (int)Math.round(pos.y);
             //Vec2 old_pos = new Vec2(pos); 
@@ -377,15 +392,6 @@ class BounceSim extends Canvas implements Runnable {
     public boolean has_tail() { return render_tail; }
     public void set_tail(boolean t) { render_tail = t; }
 
-    public void erase() {
-        //render_erase = true;
-        update(getGraphics()); 
-        //repaint();
-        //g.setColor(Color.white);
-        //if (render_circle) g.fillOval(tl_x, tl_y, size*2+5, size*2+5);
-        //else g.fillRect(tl_x, tl_y, size*2+5, size*2+5);
-    }
-
     // Use update to draw graphics instead of paint (remove screen wipes).
     public void update(Graphics g) {
         if (render_clear) {
@@ -396,16 +402,14 @@ class BounceSim extends Canvas implements Runnable {
         }
 
         if (!render_tail) {
-            g.setColor(Color.white);//getBackground());
-            if (notail_circle) g.fillOval(notail_x-1-notail_size-1, notail_y-1-notail_size-1, notail_size*2+4, notail_size*2+4);
-            else g.fillRect(notail_x-1-notail_size, notail_y-1-notail_size, notail_size*2+4, notail_size*2+4);
+            g.setColor(getBackground());
+            if (notail_circle) g.fillOval(notail_x-notail_size-5, notail_y-notail_size-5, notail_size*2+11, notail_size*2+11);
+            else g.fillRect(notail_x-1-notail_size-2, notail_y-1-notail_size-2, notail_size*2+4, notail_size*2+4);
             System.out.println("HASS");
         }
         
         int tl_x = (int)Math.round(pos.x-1-size);  // Top left position from center pixel, rounding subpixel to pixel precision. 
         int tl_y = (int)Math.round(pos.y-1-size);
-        
-        
         
         //if (render_erase) {
         //    g.setColor(Color.white);
@@ -433,7 +437,6 @@ class BounceSim extends Canvas implements Runnable {
         notail_circle = render_circle;
     }
 
-
     // Override paint to draw the red boarder and call update (os will trigger a paint occasionally).
     public void paint(Graphics g) {
         g.setColor(Color.red);
@@ -447,37 +450,37 @@ class BounceSim extends Canvas implements Runnable {
         else if (ms > SIM_DELAY_MAX) ms = SIM_DELAY_MAX;
         sim_delay = ms;
     }*/
+
+    // Templates?
+    private static int util_restrict_bounds(int num, int lower, int upper) {
+        if (num < lower) num = lower; else if (num > upper) num = upper; return num;
+    }
+    private static double util_restrict_bounds(double num, double lower, double upper) {
+        if (num < lower) num = lower; else if (num > upper) num = upper; return num;
+    }
+
     public void sim_set_tickrate(int tps) {
-        if (tps < SIM_TICKRATE_MIN) tps = SIM_TICKRATE_MIN;
-        else if (tps > SIM_TICKRATE_MAX) tps = SIM_TICKRATE_MAX;
-        sim_tickrate = tps;
-        sim_delay = (int)Math.round(1000/tps);
+        sim_tickrate = util_restrict_bounds(tps, SIM_TICKRATE_MIN, SIM_TICKRATE_MAX);
+        sim_delay = (int)Math.round(1000/sim_tickrate);
+    }
+    public void body_set_velocity_multiplier(double v) {
+        //vel.mul(BODY_VEL_INITIAL.x/vel.x*v); // Scale vel to match initial * v
+//        vel.x/BODY_VEL_INITIAL.x
+        vel = Vec2.mul((new Vec2(vel.x/vel.x*BODY_VEL_INITIAL.x, vel.y/vel.y*BODY_VEL_INITIAL.y)), util_restrict_bounds(v, BODY_VEL_MULTIPLIER_MIN, BODY_VEL_MULTIPLIER_MAX));
+        // ^ initial velocity, with current direction applied, times the multiplier.
+        //Vec2(vel.x/vel.x*BODY_VEL_INITIAL.x, vel.y/vel.y*BODY_VEL_INITIAL.y),)
+
+        // ! there is an issue here which is messing up the +/- of the velocity components.
+        // also, subpixel movement is getting disregarded somewhere
     }
     public void body_set_size(int px) {
-        if (px < BODY_SIZE_MIN) px = BODY_SIZE_MIN;
-        else if (px > BODY_SIZE_MAX) px = BODY_SIZE_MAX;
-        //erase();
         notail_size = size;
-        size = px;  
-        repaint();
-        //render_erase = true;
-        //repaint();
-        // Collision detection in the run thread should catch any boundery breaks.
-        //process_collisions();
+        size = util_restrict_bounds(px, BODY_SIZE_MIN, BODY_SIZE_MAX);
     }
-//    public void body_set_velocity(double v) {
-//        if (v < BODY_VEL_MIN) v = BODY_VEL_MIN;
-//        else if (v > BODY_VEL_MAX) v = BODY_VEL_MAX;
-//        vel.x = vel.y = v;
-//
-//    }
-//    public double body_set_velocity_multiplier() {}
-//    public double body get_velocity_multiplier() {}
 
-    public int body_get_size() { return size; }
-    public double body_get_velocty() { return vel.x; }
     public int sim_get_tickrate() { return sim_tickrate; }
-
+    public double body_get_velocity() { return Math.round(Math.sqrt(vel.x*vel.x+vel.y+vel.y)*100)/100.0; }
+    public int body_get_size() { return size; }
 }
 
 // Two dimensional vector, for velocity and position data.  Should this be doubles (do we want subpixel movement?)
@@ -496,6 +499,11 @@ class Vec2 {
         Vec2 resultant = new Vec2(augend);
         resultant.add(addend);
         return resultant;
+    }
+    public static Vec2 mul(Vec2 multiplicand, double multiplier) {
+        Vec2 product = new Vec2(multiplicand);
+        product.mul(multiplier);
+        return product;
     }
 
 }
