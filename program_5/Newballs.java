df147c7c7bcfc904524e68a0a6ae859cdd62fee7
squillwah <Swagapotato@gmail.com>
2026-03-22 17:07:43 -0400

    Consolidated GBC setup.

--- program_5/Newballs.java
+++ program_5/Newballs.java
@@ -41,59 +41,23 @@ public class Newballs extends Frame {
         setMinimumSize(dim_frame);
         setBounds(10, 10, (int)dim_frame.getWidth(), (int)dim_frame.getHeight());
         setBackground(Color.lightGray);
-
         // Create screen and control panels:
         pnl_screen = new Panel();
         pnl_screen.setLayout(new BorderLayout());
         pnl_controls = new Panel();
         pnl_controls.setLayout(new GridBagLayout());
-
         add("Center", pnl_screen);
         add("South", pnl_controls);
-
-        gbc = new GridBagConstraints();
-
-        gbc.fill = GridBagConstraints.HORIZONTAL; //?
-
-        gbc.insets = new Insets(5, 5, 0, 5);
-       
-        gbc.insets.left = 10; 
-        gbc.gridx = 0;  gbc.gridy = 0;  gbc.weightx = 0.6;  gbc.gridwidth = 1;
+        // Initialize GUI components:  
         sb_tickrate = new Scrollbar(Scrollbar.HORIZONTAL);
-        pnl_controls.add(sb_tickrate, gbc);
-        gbc.insets.right = 10;
-        gbc.gridx = 1;  gbc.gridy = 0;  gbc.weightx = 0.6;  gbc.gridwidth = 1;
-        sb_velocity = new Scrollbar(Scrollbar.HORIZONTAL);
-        pnl_controls.add(sb_velocity, gbc);
-        gbc.gridx = 5;  gbc.gridy = 0;  gbc.weightx = 0.8;  gbc.gridwidth = 1;
-        sb_size = new Scrollbar(Scrollbar.HORIZONTAL);
-        pnl_controls.add(sb_size, gbc);
-       
-        gbc.insets.top = 0; 
-        gbc.gridx = 0;  gbc.gridy = 1;  gbc.weightx = 0.3;  gbc.gridwidth = 1;
         sb_tickrate_lbl = new Label(("Tickrate"), Label.CENTER);    
-        pnl_controls.add(sb_tickrate_lbl, gbc); 
-        gbc.gridx = 1;  gbc.gridy = 1;  gbc.weightx = 0.3;  gbc.gridwidth = 1;
+        sb_velocity = new Scrollbar(Scrollbar.HORIZONTAL);
         sb_velocity_lbl = new Label(("Speed"), Label.CENTER);       
-        pnl_controls.add(sb_velocity_lbl, gbc); 
-        gbc.gridx = 5;  gbc.gridy = 1;  gbc.weightx = 0.4;  gbc.gridwidth = 1;
+        sb_size = new Scrollbar(Scrollbar.HORIZONTAL);
         sb_size_lbl = new Label(("Size"), Label.CENTER);            
-        pnl_controls.add(sb_size_lbl, gbc);
-      
-        gbc.insets.left = 5; gbc.insets.right = 5; 
-        gbc.insets.top = 5; gbc.insets.bottom = 5; 
-        gbc.fill = GridBagConstraints.BOTH; //?
-        gbc.gridx = 2;  gbc.gridy = 0;  gbc.weightx = 0.1;  gbc.gridheight = 2;
         bt_start = new Button("START");
-        pnl_controls.add(bt_start, gbc);
-        gbc.gridx = 3;  gbc.gridy = 0;  gbc.weightx = 0.1;  gbc.gridwidth = 1;
         bt_pause = new Button("PAUSE");
-        pnl_controls.add(bt_pause, gbc);
-        gbc.gridx = 4;  gbc.gridy = 0;  gbc.weightx = 0.1;  gbc.gridwidth = 1;
         bt_quit = new Button("QUIT");
-        pnl_controls.add(bt_quit, gbc);
-        
-        
         Scrollbar[] bars = {sb_size, sb_tickrate, sb_velocity};
         for (Scrollbar bar : bars) { 
             bar.setMinimum(1); 
@@ -104,6 +68,50 @@ public class Newballs extends Frame {
             bar.setBlockIncrement(50); 
             bar.setBackground(Color.gray); 
         } 
+        // GridBag Shenanigans: 
+        GridBagConstraints gbc = new GridBagConstraints();  
+        gbc.gridx = -1;  gbc.gridy = 0;  
+        gbc.fill = GridBagConstraints.HORIZONTAL;           
+        // | sb | sb | b | b | b | sb |
+        // | lb | lb |   |   |   | lb |
+        // Tickrate 
+        gbc.weightx = .2;
+        gbc.insets = new Insets(5,10,0,5);
+        gbc.gridx++;
+        pnl_controls.add(sb_tickrate, gbc);
+        gbc.gridy++; 
+        gbc.insets.top = 0;
+        pnl_controls.add(sb_tickrate_lbl, gbc);
+        gbc.gridy--;
+        // Velocity 
+        gbc.weightx = .2;
+        gbc.insets = new Insets(5,5,0,5);
+        gbc.gridx++;  
+        pnl_controls.add(sb_velocity, gbc);
+        gbc.gridy++;
+        gbc.insets.top = 0;
+        pnl_controls.add(sb_velocity_lbl, gbc);
+        gbc.gridy--;
+        // Buttons 
+        gbc.weightx = .1;
+        gbc.insets = new Insets(5,5,5,5);
+        gbc.gridheight = 2;
+        gbc.gridx++;  
+        pnl_controls.add(bt_start, gbc);
+        gbc.gridx++;  
+        pnl_controls.add(bt_pause, gbc);
+        gbc.gridx++;  
+        pnl_controls.add(bt_quit, gbc);
+        gbc.gridheight = 1;
+        // Size 
+        gbc.weightx = .3;
+        gbc.insets = new Insets(5,5,0,10);
+        gbc.gridx++;
+        pnl_controls.add(sb_size, gbc);
+        gbc.gridy++;  
+        gbc.insets.top = 0;
+        pnl_controls.add(sb_size_lbl, gbc);
+        gbc.gridy--;
         
         pnl_screen.setBackground(Color.black);
         pnl_controls.setBackground(Color.red);
